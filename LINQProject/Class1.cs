e85fd4b6e326fcdc2d5a38a95dc25c3b066cff14
Muhammad Talha Hakeem <talhahakeem295@gmail.com>
2026-01-08 07:05:12 +0500

    LINQ_in_CSharp

---
+++ LINQProject/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProject
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            int[] arr = { 21, 56, 45, 23, 78, 34, 89, 66, 77, 89, 34, 56, 99, 88, 78 };
+
+            var brr = from i in arr where i > 40 orderby i select i;
+
+            foreach (var b in brr)
+            {
+                Console.WriteLine(b + " ");
+
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ LINQProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 21, 56, 45, 23, 78, 34, 89, 66, 77, 89, 34, 56, 99, 88, 78 };
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 40)
+                    count++;
+            }
+
+            int[] brr = new int[count];
+            int index = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 40)
+                {
+                    brr[index] = arr[i];
+                    index++;
+                }
+            }
+
+            Array.Sort(brr);
+            Array.Reverse(brr);
+
+            foreach (int num in brr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
